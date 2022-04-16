179b6021a878b2d08bb0f6f29c55b8c3040f6b07
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-16 18:11:16 +0530

    push data in database of signup hash as a password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //
     public function signup(Request $request){
-        $user = new User();
         $validators=Validator::make($request->all(),[
             'name'=>'required|alpha',
             'email'=>'required',
@@ -17,7 +17,14 @@ class UserController extends Controller
             'confirm_password'=>'required_with:password|same:password|min:6'
         ]);
         if($validators->passes()){
-            print_r($request->all());
+            $user = new User();
+            $user->name=$request->name;
+            $user->email=$request->email;
+            $user->mobile=$request->mobile;
+            $user->password=Hash::make($request->password);
+            $user->save();
+
+            // print_r($request->all());
         }
         else{
             print_r($validators->errors()->all());
