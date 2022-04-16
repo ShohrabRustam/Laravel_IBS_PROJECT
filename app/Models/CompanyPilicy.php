bdf9bf5ddf1b1a30d7636d16dd0ab9935f84a313
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-17 00:53:38 +0530

    make policy controller

---
+++ app/Models/CompanyPilicy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyPilicy extends Model
+{
+    use HasFactory;
+}
