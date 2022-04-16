1ded34b81ff5b8fa80a587ce7d0f5489907bee22
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-16 17:52:19 +0530

    signup api working

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Validator;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
+    public function signup(Request $request){
+        $validators=Validator::make($request->all(),[
+            'name'=>'required|alpha',
+            'email'=>'required',
+            'mobile'=>'required|min:10|max:13',
+            'password'=>'required|min:6',
+            'confirm_password'=>'required_with:password|same:password|min:6'
+        ]);
+        if($validators->passes()){
+            print_r($request->all());
+        }
+        else{
+            print_r($validators->errors()->all());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('demo',[UserController::class,'demo']);
+Route::post('signup',[UserController::class,'signup']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
