beb3f79dd64f07fdf539642f5ece642de377550a
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-16 18:40:43 +0530

    setup the validation and print the json_decode

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,33 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     //
-    public function signup(Request $request){
-        $validators=Validator::make($request->all(),[
-            'name'=>'required|alpha',
-            'email'=>'required',
-            'mobile'=>'required|min:6000000000|max:9999999999|numeric',
-            'password'=>'required|min:6',
-            'confirm_password'=>'required_with:password|same:password|min:6'
+    public function signup(Request $request)
+    {
+        $validators = Validator::make($request->all(), [
+            'name' => 'required|alpha',
+            'email' => 'required',
+            'mobile' => 'required|min:6000000000|max:9999999999|numeric',
+            'password' => 'required|min:6',
+            'confirm_password' => 'required_with:password|same:password|min:6'
         ]);
-        if($validators->passes()){
+        if ($validators->passes()) {
             $user = new User();
-            $user->name=$request->name;
-            $user->email=$request->email;
-            $user->mobile=$request->mobile;
-            $user->password=Hash::make($request->password);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->password = Hash::make($request->password);
             $user->save();
-
-            // print_r($request->all());
-        }
-        else{
+            $arr = "{'status'=>true,'message'=>'Congratulations! Your account has been Created Successfully !!.','code'=>201}";
+        } else {
             print_r($validators->errors()->all());
+            $arr = "{'status'=>false,'message'=>$validators->errors()->all,'code'=>108}";
         }
+        echo json_decode($arr);
     }
 }
