38db8eba19da9c626033efcbb8380f90be031fc3
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-18 00:52:56 +0530

    choose the header

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -1,43 +1,47 @@
-<div class="page-wrapper">
-    <div class="nav-wrapper">
-     <div class="grad-bar"></div>
-     <nav class="navbar">
-       <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bluestar_%28bus_company%29_logo.svg/1280px-Bluestar_%28bus_company%29_logo.svg.png" alt="Company Logo">
-       <div class="menu-toggle" id="mobile-menu">
-         <span class="bar"></span>
-         <span class="bar"></span>
-         <span class="bar"></span>
-       </div>
-       <ul class="nav no-search">
-         <li class="nav-item"><a href="#">Home</a></li>
-         <li class="nav-item"><a href="#">About</a></li>
-         <li class="nav-item"><a href="#">Work</a></li>
-         <li class="nav-item"><a href="#">Careers</a></li>
-         <li class="nav-item"><a href="#">Contact Us</a></li>
-         <i class="fas fa-search" id="search-icon"></i>
-         <input class="search-input" type="text" placeholder="Search..">
-       </ul>
-     </nav>
-     </div>
-     <section class="headline">
-       <h1>Responsive Navigation</h1>
-       <p>Using CSS grid and flexbox to easily build navbars!</p>
-     </section>
-     <section class="features">
-       <div class="feature-container">
-         <img src="https://cdn-images-1.medium.com/max/2000/1*HFAEJvVOq4AwFuBivNu_OQ.png" alt="Flexbox Feature">
-         <h2>Flexbox Featured</h2>
-         <p>This pen contains use of flexbox for the headline and feature section! We use it in our mobile navbar and show the power of mixing css grid and flexbox.</p>
-       </div>
-       <div class="feature-container">
-         <img src="https://blog.webix.com/wp-content/uploads/2017/06/20170621-CSS-Grid-Layout-710x355-tiny.png" alt="Flexbox Feature">
-         <h2>CSS Grid Navigation</h2>
-         <p>While flexbox is used for the the mobile navbar, CSS grid is used for the desktop navbar showing many ways we can use both.</p>
-       </div>
-       <div class="feature-container">
-         <img src="https://blog.edx.org/wp-content/uploads/2015/09/null-1.jpg" alt="Flexbox Feature">
-         <h2>Basic HTML5</h2>
-         <p>This pen contains basic html to setup the page to display the responsive navbar.</p>
-       </div>
-     </section>
-   </div>
+<link href='//fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+<header class="header">
+    <!-- Logo -->
+    <a class="logo" href="https://www.savetherhino.org" title=" font-family: Lato, sans-serif">RHINO</a>
+    <nav>
+        <!-- MAIN MENU <div class="navicon">&#9776;</div> <div class="navicon">?</div> -->
+        <input type="checkbox" id="show-menu-input-checkbox" role="button">
+        <label for="show-menu-input-checkbox" id="main-menu-toggler" class="flex-container flex-vcenter flex-hcenter">
+            <div id="main-menu-toggler-icon" class="flex-grow"></div>
+        </label>
+        <ul class="mainMenu accordion" id="mainMenu">
+            <li><a href="https://commons.wikimedia.org/wiki/Kunstformen_der_Natur" target="_blank">life</a></li>
+            <li><a href="https://en.wikipedia.org/wiki/Douglas_Adams" target="_blank">universe</a></li>
+            <li><a href="https://archive.org/details/internetarcade" target="_blank">fun</a></li>
+            <li><a href="https://archive.org/details/prelinger" target="_blank">archive</a></li>
+            <li><a href="https://www.katetempest.co.uk" target="_blank">fight</a></li>
+            <li><a href="https://no.wikipedia.org/wiki/Siv_(mytologi)" target="_blank">love</a></li>
+            <li><a href="http://www.dankaplantrio.com" target="_blank">waves</a></li>
+            <li><a href="https://www.theguardian.com/us-news/edward-snowden" target="_blank">happening</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <p>The toggle animation is forked from <a href="https://codepen.io/CreativeJuiz/">Geoffrey Crofte</a>'s nice <a
+            href="https://codepen.io/CreativeJuiz/pen/oCBxz?editors=0010">'Menu Toggle button with flat menu' pen</a>.
+    </p>
+    <p>The CSS only stuff is based off <a
+            href="https://medialoot.com/blog/how-to-create-a-responsive-navigation-menu-using-only-css">Tony Thomas'
+            article at medialoot</a> and <a
+            href="http://stackoverflow.com/questions/18752134/reveal-and-hide-a-div-on-checkbox-condition-with-css">a
+            question at stackoverflow</a>. It's true, though, <a
+            href="https://css-tricks.com/responsive-menu-concepts/">as pointed out at CSS tricks</a>, that it's bad
+        semantics, but it's quite a simple solution, anyhow.</p>
+    <p>These pages were a great help to get a grip on flexbox magic:</p>
+    <ul>
+        <li><a
+                href="http://www.heydonworks.com/article/flexbox-grid-finesse">http://www.heydonworks.com/article/flexbox-grid-finesse</a>
+        </li>
+        <li><a href="https://codepen.io/heydon/pen/GpbQdP">https://codepen.io/heydon/pen/GpbQdP</a></li>
+        <li><a
+                href="https://philipwalton.github.io/solved-by-flexbox/">https://philipwalton.github.io/solved-by-flexbox/</a>
+        </li>
+        <li><a
+                href="http://apps.workflower.fi/css-cheats/?name=flexbox">http://apps.workflower.fi/css-cheats/?name=flexbox</a>
+        </li>
+    </ul>
+</main>
