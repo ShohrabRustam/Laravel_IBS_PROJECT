d0c47d4de730c9233065ad2502ff804cf16e8c95
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-18 21:32:44 +0530

    setting the nav for admin

---
+++ resources/views/Admin/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar">
+    <a href="{{ url('/adminHome') }}">
+    <img src="https://static.wixstatic.com/media/c69ef5_bda0347e97ef4e4c8fa0119903a7f1c0~mv2.png/v1/fit/w_2500,h_1330,al_c/c69ef5_bda0347e97ef4e4c8fa0119903a7f1c0~mv2.png"
+        alt="Company Logo"></a>
+    <div class="menu-toggle" id="mobile-menu">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+    </div>
+    <ul class="nav no-search">
+        <li class="nav-item"><a href="{{ url('/adminHome') }}">Home</a></li>
+        <li class="nav-item"><a href="{{ url('/insuranceRequest') }}">I_Request</a></li>
+        <li class="nav-item"><a href="{{ url('/claimRequest') }}">C_Request</a></li>
+        <li class="nav-item"><a href="{{ url('/adminLogin') }}">Login</a></li>
+    </ul>
+</nav>
