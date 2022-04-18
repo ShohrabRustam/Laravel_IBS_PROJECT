f5d4f968a2b8c1262a2d3b0af336839d81eee82d
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-18 16:47:56 +0530

    make a form for help

--- resources/views/Users/help.blade.php
+++ resources/views/Users/help.blade.php
@@ -1,8 +1,74 @@
 @extends('Layout.master')
 @section('title')
-Help
+    Help
 @endsection
 
 @section('section')
-<h1>Help</h1>
+    <div class="container py-3">
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <span class="anchor" id="formContact"></span>
+                <hr class="my-5">
+                <!-- form contact -->
+                <div class="card card-outline-secondary">
+                    <div class="card-header">
+                        <h3 class="mb-0">Send Us Message For Any Help</h3>
+                    </div>
+                    <div class="card-body">
+                        <form autocomplete="off" class="form" role="form">
+                            <fieldset>
+                                <label class="mb-0" for="name2">Name</label>
+                                <div class="row mb-1">
+                                    <div class="col-lg-12">
+                                        <input class="form-control" id="name2" name="name2" required="" type="text">
+                                    </div>
+                                </div>
+                                <label class="mb-0" for="email2">Email</label>
+                                <div class="row mb-1">
+                                    <div class="col-lg-12">
+                                        <input class="form-control" id="email2" name="email2" required="" type="text">
+                                    </div>
+                                </div>
+                                <label class="mb-0" for="message2">Message</label>
+                                <div class="row mb-1">
+                                    <div class="col-lg-12">
+                                        <textarea class="form-control" id="message2" name="message2" required="" rows="6"></textarea>
+                                    </div>
+                                </div>
+                                <button class="btn btn-secondary btn-lg float-right" type="submit">Send Message</button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div><!-- /form contact -->
+            </div>
+            <!--/col-->
+        </div>
+
+    </div>
+    <!-- Scroll to Top -->
+    <a id="scroll-to-top" href="#" class="btn btn-primary btn-lg" role="button" title="Return to top of page"
+        data-toggle="tooltip" data-placement="left"><i class="fa fa-arrow-up"></i></a>
+
+
+    <style>
+        .helpHead {
+            color: navy;
+        }
+
+        body {
+            margin: 0;
+            background-color: #ecfab6;
+        }
+
+        /* Scroll to Top */
+        #scroll-to-top {
+            cursor: pointer;
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            display: none;
+        }
+
+    </style>
 @endsection
