c32a1f02baf9fbcc613b444e879a20c7913ba48f
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-23 23:40:23 +0530

    sending the policies in controller for health life and vichels

--- app/Http/Controllers/InsuranceController.php
+++ app/Http/Controllers/InsuranceController.php
@@ -3,24 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\CompanyPolicy;
 
 class InsuranceController extends Controller
 {
 
     public function _bike(){
-        return view('Users.bikeInsurance');
+        if(Session::has('user')){
+            $policies = CompanyPolicy::where('policytype','Bike')->get();
+        return view('Users.bikeInsurance')->with('policies',$policies);
+        }else{
+            return redirect('/login');
+        }
     }
 
     public function _car(){
-        return view('Users.carInsurance');
+        if(Session::has('user')){
+        $policies = CompanyPolicy::where('policytype','Car')->get();
+        return view('Users.carInsurance')->with('policies',$policies);
+        }
+        else{
+            return redirect('/login');
+        }
     }
 
     public function _life(){
-        return view('Users.healthInsurance');
+        if(Session::has('user')){
+        $policies = CompanyPolicy::where('policytype','Health')->get();
+        return view('Users.lifeInsurance')->with('policies',$policies);
+        }
+        else{
+            return redirect('/login');
+        }
     }
 
     public function _health(){
-        return view('Users.lifeInsurance');
+        if(Session::has('user')){
+            $policies = CompanyPolicy::where('policytype','Health')->get();
+            return view('Users.healthInsurance')->with('policies', $policies);
+        }else{
+            return redirect('/login');
+        }
     }
     //
 }
